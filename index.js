d11f97dafbf73e589a1da4c3def78b28de10ed97
kamaboko1 <github65@hotmail.com>
2019-06-03 13:03:34 -0700

    added get and create user

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // implement your API here
+
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+server.use(express.json());
+
+// GET all users
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(allFriends => {
+        res.json(allFriends);
+    })
+    .catch(({code, message}) => {
+        res.status(code).json({err: message})
+    })
+});
+
+
+// CREATE a user
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+
+    db.insert(newUser)
+    .then(addedUser => {
+        res.status(200).json(addedUser)
+    })
+    .catch(({code, message}) => {
+        res.status(code).json({err: message})
+    })
+})
