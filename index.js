a2c43b93caa39c1b956f89043fa9acd4a4d56380
kamaboko1 <github65@hotmail.com>
2019-06-03 13:09:57 -0700

    added delete and update user

--- index.js
+++ index.js
@@ -31,3 +31,38 @@ server.post('/api/users', (req, res) => {
         res.status(code).json({err: message})
     })
 })
+
+// DELETE a user
+server.delete('/api/users/:id', (req, res) => {
+    const {id} = req.params
+
+    db.remove(id)
+    .then(removedUser => {
+        if(removedUser){
+            res.json(removedUser)
+        }else{
+            res.status(404).json({err: 'Failed process.  Check id'})
+        }
+    })
+    .catch(({code, message}) => {
+        res.status(code).json({err: message})
+    })
+})
+
+// UPDATE a user
+server.put('/api/users/:id', (req, res) => {
+    const {id} = req.params
+    const user = req.body
+
+    db.update(id, user)
+    .then(updateUser => {
+        if(updateUser){
+            res.json(updateUser)
+        }else{
+            res.status(404).json({err: 'Failed to update user'})
+        }
+    })
+    .catch(({code, message}) => {
+        res.status(code).json({err: message})
+    })
+})
